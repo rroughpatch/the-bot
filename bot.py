65924ba2aa54e406e3ec95dce5c1427c8142471b
garland <contact@rrough.co>
2023-10-10 14:24:19 -0500

    init

--- bot.py
+++ bot.py
@@ -1,10 +1,15 @@
 import asyncio
 import discord
 from discord.ext import commands
-from utils import delete_message, get_timestamp
+import utils as utils
+import requests
+import os
 
 intents = discord.Intents.all()
 bot = commands.Bot(command_prefix='!', intents=intents)
+
+song_queue = []
+current_index = 0
 
 
 @bot.event
@@ -15,13 +20,11 @@ async def on_ready():
 @bot.event
 async def on_voice_state_update(member, before, after):
     guild = member.guild
-
     if before.channel != after.channel:
         if before.channel != after.channel:
             if after.channel and not member.bot:
                 voice_channel = after.channel
-                formatted_timestamp = get_timestamp()
-
+                formatted_timestamp = utils.get_timestamp()
                 for channel in guild.voice_channels:
                     if channel == voice_channel:
                         text_channel = guild.get_channel(channel.id)
@@ -29,13 +32,11 @@ async def on_voice_state_update(member, before, after):
                         message = await text_channel.send(
                             f'{formatted_timestamp} **{member.display_name}** Connected!'
                         )
-
-                        await delete_message(message, 20)
+                        await utils.delete_message(message, 20)
 
             elif before.channel and not member.bot:
                 voice_channel = before.channel
-                formatted_timestamp = get_timestamp()
-
+                formatted_timestamp = utils.get_timestamp()
                 for channel in guild.voice_channels:
                     if channel == voice_channel:
                         text_channel = guild.get_channel(channel.id)
@@ -43,8 +44,105 @@ async def on_voice_state_update(member, before, after):
                         message = await text_channel.send(
                             f'{formatted_timestamp} **{member.display_name}** Disconnected!'
                         )
+                        await utils.delete_message(message, 20)
 
-                        await delete_message(message, 20)
+
+@bot.command()
+async def play(ctx, *, track_name):
+    """Add a track to the queue and play if it's the first track."""
+    global current_index
+
+    if not ctx.voice_client:
+        await utils.connect_to_voice_channel(ctx)
+
+    if utils.is_valid_url(track_name):
+        file_path = await utils.download_audio_from_url(track_name)
+    else:
+        file_path = await utils.process_file_attachment(ctx, track_name)
+
+    if file_path:
+        song_queue.append(file_path)
+
+        if not ctx.voice_client.is_playing():
+            await play_next(ctx)
+
+        utils.schedule_temp_file_cleanup(ctx, file_path)
+
+
+async def play_next(ctx):
+    """Play the next track in the queue if available."""
+    global current_index
+
+    if current_index < len(song_queue):
+        track_name = song_queue[current_index]
+        source = discord.FFmpegPCMAudio(executable="ffmpeg", source=track_name)
+
+        ctx.voice_client.play(source, after=lambda e: play_next(ctx))
+        current_index += 1
+
+        metadata = utils.get_mp3_metadata(track_name)
+
+        if metadata:
+            embed = discord.Embed(title=f"[{metadata['title']}]({track_name})", color=discord.Color.green())
+            embed.add_field(name="Artist", value=metadata.get("artist", "Unknown"))
+            embed.add_field(name="Album", value=metadata.get("album", "Unknown"))
+
+            await ctx.send(embed=embed, delete_after=20)
+    else:
+        await ctx.voice_client.disconnect()
+        current_index = 0  # Reset the index
+
+
+@bot.command()
+async def skip(ctx):
+    """Skip the currently playing track."""
+    if ctx.voice_client and ctx.voice_client.is_playing():
+        ctx.voice_client.stop()
+        await ctx.send("Skipped the current track.")
+    else:
+        await ctx.send("I'm not playing anything right now.")
+
+
+@bot.command()
+async def queue(ctx):
+    """List the tracks in the queue."""
+    if queue:
+        queue_list = '\n'.join(song_queue)
+        await ctx.send(f"**Queue:**\n{queue_list}")
+    else:
+        await ctx.send("The queue is empty.")
+
+
+@bot.command()
+async def clear_queue(ctx):
+    """Clear the queue of tracks."""
+    global current_index, song_queue
+    queue.clear()
+    current_index = 0
+    if ctx.voice_client:
+        ctx.voice_client.stop()
+    await ctx.send("The queue has been cleared.")
+
+
+@bot.command()
+async def join(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+
+        if not ctx.voice_client or not ctx.voice_client.is_connected():
+            await channel.connect()
+        else:
+            await ctx.send("I'm already in a voice channel.")
+    else:
+        await ctx.send("You need to be in a voice channel to use this command.")
+
+
+@bot.command()
+async def leave(ctx):
+    if ctx.voice_client:
+        await ctx.voice_client.disconnect()
+    else:
+        await ctx.send("I'm not in a voice channel right now.")
 
 
 if __name__ == '__main__':
--- utils.py
+++ utils.py
@@ -1,5 +1,46 @@
 import asyncio
 from datetime import datetime
+import eyed3
+import os
+import discord
+from urllib.parse import urlparse, urlunparse
+import requests
+
+
+def format_metadata(metadata, track_name):
+    formatted_metadata = f"**Now playing:** {metadata['title']} "
+    formatted_metadata += f"by {metadata['artist']}\n"
+    formatted_metadata += f"**Album:** {metadata['album']}\n"
+
+    return formatted_metadata
+
+
+def get_mp3_metadata(file_path):
+    audiofile = eyed3.load(file_path)
+
+    if audiofile.tag:
+        title = audiofile.tag.title
+        artist = audiofile.tag.artist
+        album = audiofile.tag.album
+        track_number = audiofile.tag.track_num[0]
+
+        return {
+            'title': title,
+            'artist': artist,
+            'album': album,
+            'track_number': track_number,
+        }
+    return None
+
+
+def schedule_temp_file_cleanup(ctx, file_path):
+    def cleanup_temp_file():
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
+    ctx.voice_client.source = discord.PCMVolumeTransformer(ctx.voice_client.source)
+    ctx.voice_client.source.volume = 0.6
+    ctx.voice_client.source.after = cleanup_temp_file
 
 
 async def delete_message(message, delay):
@@ -10,3 +51,57 @@ async def delete_message(message, delay):
 def get_timestamp():
     timestamp = int(datetime.now().timestamp())
     return f'<t:{timestamp}:R>'
+
+
+async def connect_to_voice_channel(ctx):
+    if ctx.author.voice:
+        channel = ctx.author.voice.channel
+        await channel.connect()
+    else:
+        await ctx.send("You need to be in a voice channel to use this command.")
+
+
+def is_valid_url(url):
+    return url.startswith("http")
+
+
+async def download_audio_from_url(url):
+    url_parts = urlparse(url)
+    file_name = os.path.basename(url_parts.path)
+    simplified_file_name = ''.join(e for e in file_name if e.isalnum() or e.isspace())
+    file_path = f"temp/{simplified_file_name}"
+
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(file_path, 'wb') as file:
+            file.write(response.content)
+        return file_path
+    return None
+
+
+async def process_file_attachment(ctx, file_name):
+    # Check if the message has attachments
+    if len(ctx.message.attachments) == 0:
+        await ctx.send("Please provide an audio file attachment.")
+        return None
+
+    # Get the first attachment
+    attachment = ctx.message.attachments[0]
+
+    # Check if the attachment is an audio file (you can add more audio file extensions if needed)
+    allowed_extensions = ['.mp3', '.wav', '.ogg']
+    if not any(attachment.filename.endswith(ext) for ext in allowed_extensions):
+        await ctx.send("Unsupported file format. Please provide an audio file (e.g., .mp3).")
+        return None
+
+    # Download the attachment and save it as a temporary file
+    file_path = f"temp/{attachment.filename}"
+    await attachment.save(file_path)
+
+    return file_path
+
+
+def clean_audio_url(audio_url):
+    parsed_url = urlparse(audio_url)
+    clean_url = urlunparse(parsed_url._replace(query=''))  # Remove query parameters
+    return clean_url
